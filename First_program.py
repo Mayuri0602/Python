595c8e61b16bdeab622e9c8aea5044a877a41ac6
Mayuri0602 <mayurimaheshwari760@gmail.com>
2025-01-29 15:22:47 +0530

    Add files via upload
    basic program

---
+++ First_program.py
@@ -0,0 +1,17 @@
+print("Hello, World!")
+print("My name is Pranjal.", "I'm 23 years old.") 
+
+
+name="Preeti"
+age=22
+age2=age
+
+
+print("My name is: ",name)
+print(age)
+print(age2)
+
+
+print(type(name))
+print(type(age))
+
