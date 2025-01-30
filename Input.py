a47859b5fdd5bf954b674adabd9fb5b382e7a2f1
Mayuri0602 <mayurimaheshwari760@gmail.com>
2025-01-30 19:00:58 +0530

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,9 @@
+a=input("Enter your name:")
+b=input("Enter your age:")
+c=input("Enter the city name where you born:")
+d=input("Enter your annual income:")
+
+print(a)
+print(b)
+print("Welcome to",c)
+print(d)
