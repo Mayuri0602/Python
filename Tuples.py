d63dda1af1053f96ec2a0f919baa6b91720f31ee
Mayuri0602 <mayurimaheshwari760@gmail.com>
2025-02-02 16:50:19 +0530

    Add files via upload

---
+++ Tuples.py
@@ -0,0 +1,8 @@
+tup1=()      #empty tuple
+tup2=(56,89,"hello","world")
+
+print(type(tup1))
+print(tup2)
+
+print(tup2.index("hello"))      #returns index of first occurrence
+print(tup2[2:4])               #slicing
