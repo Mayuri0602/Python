8a5f6d415e0fd4b007bb459edd232ea48666932c
Mayuri0602 <mayurimaheshwari760@gmail.com>
2025-02-09 10:28:31 +0530

    Add files via upload

---
+++ Loops.py
@@ -0,0 +1,41 @@
+#qs1
+# i=20
+# while i>=10:
+#     print(i)
+#     i-=1
+
+
+#qs2
+# i=1
+# while i<=50:
+#     print(i)
+#     i+=1    
+    
+
+#qs3
+# n=int(input("Enter a number:"))
+# i=1
+# while i<=10:
+#     print(n*i)
+#     i+=1
+
+
+#qs4
+# nums=[1,4,9,16,25,36,49,64,81,90,"hello","world"]
+# i=0
+# while i<len(nums):
+#     print(nums[i])
+#     i+=1
+
+
+#qs5
+# num=(1,3,5,7,9,11,13,15,17,19)
+# x=15
+# i=0
+# while i<len(num):
+#     if(num[i]==x):
+#         print("found at idx",i)
+#     i+=1
+
+
+#qs6
