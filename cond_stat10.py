8b0ac8407375f9bbe4b620884971ee6ecee7eea8
Mayuri0602 <mayurimaheshwari760@gmail.com>
2025-03-21 12:59:16 +0530

    number lies in a specific range

---
+++ cond_stat10.py
@@ -0,0 +1,5 @@
+n=int(input("Enter a number: "))
+if(n>10 and n<50):
+    print("number lies in between the range of 10 and 50")
+else:
+    print("number does not lie in the range of 10 and 50")    
