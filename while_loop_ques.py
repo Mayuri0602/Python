a5c8732898dd2bc219a8fd58dc91caec304533c1
Mayuri0602 <mayurimaheshwari760@gmail.com>
2025-04-13 13:30:35 +0530

    question practice using while loop

---
+++ while_loop_ques.py
@@ -0,0 +1,32 @@
+# i = 1234
+# odd = 0
+# even = 0
+# while(i>0):
+#     r=i%10
+#     if(i%2==0):
+#         even=even+r
+#     else:
+#         odd=odd+r
+#     i=i//10
+# print(even)
+# print(odd) 
+
+
+
+#user defined program
+
+# i=int(input("Enter first number: "))
+# n=int(input("Enter last number: "))
+# while(i<=n):
+#     print(i)
+#     i=i+1
+
+
+#sum of square of a given number
+
+i=123
+sum=0
+while(i>0):
+    sum=sum+(i%10)+(i%10)
+    i=i//10
+print(sum)    
